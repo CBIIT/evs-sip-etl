f15dd593621391ea5b753692900e98cfd3dddc65
JoonLeeNIH <joon.lee2@nih.gov>
2022-02-04 18:01:37 -0500

    Database clearing is now handled by main instead of load; database is only cleared in development environments

--- src/load.js
+++ src/load.js
@@ -3,33 +3,76 @@ import neo4j from 'neo4j-driver';
 const load = async (transformedData) => {
   // Data
   const node = transformedData.node;
+  const relationships = transformedData.relationships;
 
   // Neo4j connection
   const uri = process.env.NEO4J_URI;
   const user = process.env.NEO4J_USER;
   const pass = process.env.NEO4J_PASS;
   const driver = neo4j.driver(uri,neo4j.auth.basic(user, pass));
+
+  // Clear, for testing purposes
+  loadNode(node, driver).then(() => {
+    loadRelationships(relationships, driver);
+  }).then(() => {
+    driver.close();
+  });
+};
+
+/**
+ * Loads the Node
+ */
+const loadNode = async (node, driver) => {
   const session = driver.session();
+  session.run(
+    `CREATE (n:node {
+      handle: '${node.handle}',
+      model: '${node.model}',
+      nanoid: '${node.nanoid}'
+    })`
+  ).then(() => {
+    session.close();
+  });
+};
 
-  try {
-    // Clear, for testing purposes
-    await session.run(
-      'MATCH (n) DELETE n'
-    );
+/**
+ * Loads Relationships
+ */
+const loadRelationships = async (relationships, driver) => {
+  relationships.forEach(async (relationship) => {
+    const session = driver.session();
 
-    // Create Node
-    await session.run(
-      `CREATE (n:node {
-        handle: '${node.handle}',
-        model: '${node.model}',
-        nanoid: '${node.nanoid}'
+    // Create Relationship node
+    session.run(
+      `CREATE (r:relationship {
+        handle: '${relationship.handle}',
+        is_required: '${relationship.is_required}',
+        model: '${relationship.model}',
+        multiplicity: '${relationship.multiplicity}',
+        nanoid: '${relationship.nanoid}'
       })`
-    );
-  } finally {
-    await session.close();
-  }
-
-  await driver.close();
+    ).then(() => {
+    /*
+      session.run(`
+        MATCH
+          (r:relationship),
+          (n:node)
+        WHERE r.handle = '${relationship.handle}' AND n.handle = '${relationship.src}'
+        CREATE (r)-[e:RELTYPE {name: 'has_src'}]->(n)
+      `);
+    }).then(() => {
+      session.run(`
+        MATCH
+          (r:relationship),
+          (n:node)
+        WHERE r.handle = '${relationship.handle}' AND n.handle = '${relationship.dst}'
+        CREATE (r)-[e:RELTYPE {name: 'has_dst'}]->(n)
+      `);
+    }).then(() => {
+    */
+      session.close();
+    });
+  });
 };
 
 export default load;
--- src/main.js
+++ src/main.js
@@ -1,18 +1,54 @@
 import { readdirSync } from 'fs';
 import path from 'path';
 import extract from './extract.js';
+import neo4j from 'neo4j-driver';
 import transform from './transform.js';
 import load from './load.js';
 
 const main = () => {
   const filePaths = filesGenerator();
 
+  if (process.env.IS_DEV) {
+    clearDatabase().then(() => {
+      etl(filePaths);
+    });
+  } else {
+    etl(filePaths);
+  }
+};
+
+/**
+ * Performs data extraction, transformation, and loading
+ * 
+ * @param {Array} filePaths Full paths of files to extract
+ */
+const etl = (filePaths) => {
   // Go through each file
   for (const filePath of filePaths) {
     const parsedFile = extract(filePath);
     const transformedData = transform(parsedFile);
     load(transformedData);
   }
+}
+
+/**
+ * Clears the database for testing purposes
+ */
+const clearDatabase = async () => {
+  // Neo4j connection
+  const uri = process.env.NEO4J_URI;
+  const user = process.env.NEO4J_USER;
+  const pass = process.env.NEO4J_PASS;
+  const driver = neo4j.driver(uri,neo4j.auth.basic(user, pass));
+  const session = driver.session();
+
+  session.run(
+    'MATCH (n) DETACH DELETE n'
+  ).then(() => {
+    session.close();
+  }).then(() => {
+    driver.close();
+  });
 };
 
 /**
