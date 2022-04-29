b1f73728d1b592b937e4e4d28229338004dafa8f
Joon Lee <joon.lee2@nih.gov>
2022-04-29 15:08:01 -0400

    Creates a node MDF file from the PCDC spreadsheet

--- src/actionTypes/makeMdf/makeMdfPcdc.js
+++ src/actionTypes/makeMdf/makeMdfPcdc.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import XLSX from 'xlsx';
-import Node from '../../lib/Node.js';
+import yaml from 'js-yaml';
 
 const makeMdfPcdc = async () => {
   const nodes = {};
@@ -8,22 +8,30 @@ const makeMdfPcdc = async () => {
 
   for (const row of rows) {
     const category = row.Project;
+    const nodeName = row['PCDC Table PT'];
+    const propName = row['NCIt PT'];
+    const propType = row['Has PCDC Data Type PT'];
 
     // Provision a new category
     if (!nodes.hasOwnProperty(category)) {
       nodes[category] = {};
     }
 
-    //console.log(row);
+    // Provision a new Node
+    if (!nodes[category].hasOwnProperty(nodeName)) {
+      nodes[category][nodeName] = {};
+    }
 
-    let node = new Node({
-      handle: row['PCDC Table PT'],
-      model: 'PCDC',
-    });
+    // Provision a new Property
+    if (propType) {
+      nodes[category][nodeName][propName] = {
+        type: propType,
+      };
+    }
   }
-  console.log(nodes);
 
-  //fs.writeFile('output/pcdc_model_file.yaml');
+  // Transform and dump to YAML file
+  await writeYamlFile(await transformNodeMap(nodes));
 };
 
 /**
@@ -44,6 +52,49 @@ const rowsGenerator = function* () {
   } catch (err) {
     console.log(err);
   }
+};
+
+/**
+ * Transforms node map in preparation for YAML dump
+ * @param {Object} map  The node map to transform
+ * @returns {Object} The transformed node map
+ */
+const transformNodeMap = async (map) => {
+  const newMap = {
+    Nodes: {},
+    Relationships: {},
+  };
+
+  // Transform categories and nodes to nodes
+  for (const category in map) {
+    // Build list of nodes
+    for (const nodeName in map[category]) {
+      const node = {
+        Category: category,
+        Props: [],
+      };
+
+      // Build node's list of properties
+      for (const propName in map[category][nodeName]) {
+        node.Props.push(propName);
+      }
+
+      newMap.Nodes[nodeName] = node;
+    }
+  }
+
+  return newMap;
+};
+
+/**
+ * Writes the provided JSON object to a YAML file
+ * 
+ * @param {Object} jsObj The object to write to YAML
+ * @param {String} path The path and filename of the output file
+ */
+const writeYamlFile = async (jsObj, path) => {
+  const dump = yaml.dump(jsObj);
+  fs.writeFileSync('output/pcdc-model-file.yaml', dump, 'utf8');
 };
 
 export default makeMdfPcdc;
