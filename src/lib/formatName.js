ccf44f1802506064485cd77908eb0fd4b16a0f11
Joon Lee <joon.lee2@nih.gov>
2022-06-02 12:05:56 -0400

    formatName uses a more thorough regex for replace

--- src/lib/formatName.js
+++ src/lib/formatName.js
@@ -7,14 +7,20 @@
 const formatName = (name) => {
   // Handle non-strings
   if (typeof name !== 'string') {
-    throw new Error(`Provided value ${name} is not a string!`);
+    //throw new Error(`Provided value ${name} is not a string!`);
+    return name;
   }
 
-  // Replace each space or series of consecutive spaces with an underscore
-  name = name.toLowerCase();
-  name = name.replaceAll(/[\-\/\\ ]+/g, '_');
+  // Replace leading non-alphabetical or non-underscore characters with an underscore
+  name = name.replaceAll(/^[^A-Za-z_]+/g, '_');
 
-  return name;
+  // Replace non-alphanumeric, non-underscore, and non-period characters with an underscore
+  name = name.replaceAll(/[^A-Za-z0-9_.]+/g, '_');
+
+  // Collapse underscores
+  name = name.replaceAll(/_+/g, '_');
+
+  return name.toLowerCase();
 };
 
 export default formatName;
