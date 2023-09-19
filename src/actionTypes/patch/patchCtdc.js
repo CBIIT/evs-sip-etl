593e0b17f05b177448ac62c90e8e2c27e81ff966
Joon Lee <joon.lee2@nih.gov>
2023-09-19 13:41:32 -0400

    CTDC patcher adds NCIt codes to permissable values

--- src/actionTypes/patch/patchCtdc.js
+++ src/actionTypes/patch/patchCtdc.js
@@ -20,49 +20,22 @@ const patchCtdc = async () => {
   for (const nodeName in nodes) {
     const node = nodes[nodeName];
     const nodeNcitCode = node.n_n_code;
-    const nodeSession = driver.session();
     const properties = node.properties;
 
-    try {
-      console.log(`Assigning NCIt code ${nodeNcitCode} to node "${nodeName}"...`);
-
-      const result = await nodeSession.run(
-        'MATCH (n:node) WHERE n.handle=$nodeName AND n.model=$model SET n.ncit_code=$ncitCode RETURN n.handle, n.ncitCode;',
-        {
-          model: 'CTDC',
-          ncitCode: nodeNcitCode,
-          nodeName: nodeName,
-        }
-      );
-      const record = result.records[0];
-
-      console.log(`Assigned NCIt code ${record.get('n.ncit_code')} to node ${record.get('n.handle')}`);
-    } finally {
-      await nodeSession.close()
-    }
+    await patchNode(nodeNcitCode, nodeName);
 
     for (const property of properties) {
       const propertyName = property.p_name;
       const propertyNcitCode = property.p_n_code;
-      const propertySession = driver.session();
+      const values = property.values;
 
-      try {
-        console.log(`Assigning NCIt code ${propertyNcitCode} to property ${nodeName}.${propertyName}...`);
+      await patchProperty(propertyNcitCode, nodeName, propertyName);
 
-        const result = await propertySession.run(
-          'MATCH (p:property) MATCH (p)<--(n:node) WHERE n.handle=$nodeName AND p.handle=$propertyName AND n.model=$model SET p.ncit_code=$ncitCode RETURN p.handle, p.ncitCode, n.handle;',
-          {
-            model: 'CTDC',
-            ncitCode: propertyNcitCode,
-            nodeName: nodeName,
-            propertyName: propertyName,
-          }
-        );
-        const record = result.records[0];
+      for (const value of values) {
+        const valueName = value.v_name;
+        const valueNcitCode = value.v_n_code;
 
-        console.log(`Assigned NCIt code ${record.get('p.ncit_code')} to property ${record.get('n.handle')}.${record.get('p.handle')}`);
-      } finally {
-        await propertySession.close()
+        await patchValue(valueNcitCode, nodeName, propertyName, valueName);
       }
     }
   }
@@ -70,9 +43,118 @@ const patchCtdc = async () => {
   console.log('Finished patching NCIt codes');
 
   // on application exit:
-  await driver.close()
+  await driver.close();
 
   return;
+};
+
+/**
+ * @param {string} ncitCode The node's NCIt code
+ * @param {string} nodeName The node's name
+ */
+const patchNode = async (ncitCode, nodeName) => {
+  const session = driver.session();
+
+  try {
+    console.log(`Assigning NCIt code ${ncitCode} to node "${nodeName}"...`);
+
+    const result = await session.run(
+      `
+        MATCH (n:node)
+        WHERE n.handle=$nodeName
+          AND n.model=$model
+        SET n.ncit_code=$ncitCode
+        RETURN n.handle, n.ncit_code;
+      `,
+      {
+        model: 'CTDC',
+        ncitCode: ncitCode,
+        nodeName: nodeName,
+      }
+    );
+    const record = result.records[0];
+
+    console.log(`Assigned NCIt code ${record.get('n.ncit_code')} to node ${record.get('n.handle')}`);
+  } finally {
+    await session.close();
+  }
+};
+
+/**
+ * @param {string} ncitCode The property's NCIt code
+ * @param {string} nodeName The node's name
+ * @param {string} propertyName The property's name
+ */
+const patchProperty = async (ncitCode, nodeName, propertyName) => {
+  const session = driver.session();
+
+  try {
+    console.log(`Assigning NCIt code ${ncitCode} to property ${nodeName}.${propertyName}...`);
+
+    const result = await session.run(
+      `
+        MATCH (p:property)
+          MATCH (p)<--(n:node)
+        WHERE n.handle=$nodeName
+          AND p.handle=$propertyName
+          AND n.model=$model
+        SET p.ncit_code=$ncitCode
+        RETURN p.handle, p.ncit_code, n.handle;
+      `,
+      {
+        model: 'CTDC',
+        ncitCode: ncitCode,
+        nodeName: nodeName,
+        propertyName: propertyName,
+      }
+    );
+    const record = result.records[0];
+
+    console.log(`Assigned NCIt code ${record.get('p.ncit_code')} to property ${record.get('n.handle')}.${record.get('p.handle')}`);
+  } finally {
+    await session.close();
+  }
+};
+
+/**
+ * @param {string} ncitCode The value's NCIt code
+ * @param {string} nodeName The node's name
+ * @param {string} propertyName The property's name
+ * @param {stirng} valueName The value's name
+ */
+const patchValue = async (ncitCode, nodeName, propertyName, valueName) => {
+  const session = driver.session();
+
+  try {
+    console.log(`Assigning NCIt code ${ncitCode} to value ${nodeName}.${propertyName}.${valueName}...`);
+
+    const result = await session.run(
+      `
+        MATCH (p:property)
+          MATCH (p)<--(n:node)
+          MATCH (vs:value_set)<--(p)
+          MATCH (v:term)<--(vs)
+        WHERE n.model=$model
+          AND n.handle=$nodeName
+          AND p.handle=$propertyName
+          AND v.value=$valueName
+        SET v.ncit_code=$ncitCode
+        RETURN v.ncit_code, n.handle, p.handle, v.value;
+      `,
+      {
+        model: 'CTDC',
+        ncitCode: ncitCode,
+        nodeName: nodeName,
+        propertyName: propertyName,
+        valueName: valueName,
+      }
+    );
+    const record = result.records[0];
+
+    console.log(`Assigned NCIt code ${record.get('v.ncit_code')} to value ${record.get('n.handle')}.${record.get('p.handle')}.${record.get('v.value')}`);
+  } finally {
+    await session.close();
+  }
 };
 
 export default patchCtdc;
